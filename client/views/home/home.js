8305cd2e119cf036cc7fd71e55ab86f987f35a60
DrDanPrice <dr.dan.price@gmail.com>
2016-05-05 05:54:18 -0500

    starting on helpers for picking pollutants

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,3 +1,8 @@
+var pollut = new ReactiveVar('OZONE');
+//can selected times be global?
+var startEpoch = new ReactiveVar(moment().subtract(1439, 'minutes').unix()); //24 hours ago - seconds
+var endEpoch = new ReactiveVar(moment().unix());
+
 Template.home.onRendered(function () {
 
     var latude = 29.721; //Houston
@@ -22,14 +27,19 @@ Template.home.onRendered(function () {
     });
     Meteor.subscribe('gridvals');
     GridValues.find({$and: [ { gridsizex : 10 }, { gridsizey : 10 }]}).observeChanges({
+    //GridValues.find().observeChanges({
+    //
+
+    //then need to figure out how to assign right range
         added: function (id, pt) {
+          var val = pt[pollut.get()];
           var O3val = pt.OZONE;
           var AQIcolor = '#b3cce6';
           if (O3val>0) {AQIcolor='#3FAE4A'};
           if (O3val>40) {AQIcolor='#F6EC26'};
           if (O3val>48) {AQIcolor='#ffcce6'};
           if (O3val>56) {AQIcolor='#ff0000'};
-          var circle = L.circle([pt.loc[1], pt.loc[0]], 4550, {
+          var circle = L.circle([pt.loc[1], pt.loc[0]], 5000, {
             color: AQIcolor,
             fillColor: AQIcolor,
             fillOpacity: 0.2
@@ -49,4 +59,23 @@ Template.home.onRendered(function () {
 
     L.tileLayer.provider('OpenStreetMap.DE').addTo(AQmap);
 
+});
+Template.home.helpers({
+    pollutant: function () {
+        return 'OZONE';
+    },
+
+    selectedDate: function () {
+        return moment.unix(endEpoch.get()).format('YYYY-MM-DD');
+    }
+});
+
+Template.home.events({
+    'change #datepicker': function (event) {
+        startEpoch.set(moment(event.target.value, 'YYYY-MM-DD').unix());
+        endEpoch.set(moment.unix(startEpoch.get()).add(1439, 'minutes').unix()); //always to current?
+    },
+    'change #pollutpick': function () {
+        pollut.set(event.target.value)
+    }
 });
